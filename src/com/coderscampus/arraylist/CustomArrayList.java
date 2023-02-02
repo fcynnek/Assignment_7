3da47b0a3fc889a621bbb2c08c982fe3e761f782
fcynnek <kchengfung@gmail.com>
2023-02-01 20:31:11 -0500

    WIP add method

---
+++ src/com/coderscampus/arraylist/CustomArrayList.java
@@ -0,0 +1,97 @@
+package com.coderscampus.arraylist;
+
+public class CustomArrayList<T> implements CustomList<T> {
+	
+	int capacity = 10; // initial max capacity of items array
+	
+	Object[] items = new Object[capacity];
+	
+	int sizeOfList = 0;
+
+	@Override
+	public boolean add(T item) {
+		
+		items[sizeOfList] = item;
+		sizeOfList++;
+		
+		if (sizeOfList == capacity) {
+			capacity = 2 * capacity;
+			
+			Object[] newItems = new Object[capacity];
+			int i = 0;
+				
+			while (i <= sizeOfList-1) {
+				newItems[i] = items[i];
+				i++;
+			}
+						
+			items = newItems;
+		} 
+		
+		return false;
+	}
+
+	
+	@Override
+	public boolean add(int index, T item) throws IndexOutOfBoundsException {
+		
+		add(item);
+		
+//		if (index != sizeOfList && index <= sizeOfList) {
+			
+			Object[] tempItems = new Object[sizeOfList];
+			int i = 0;
+			
+			while (index <= sizeOfList) {
+				items[index-1] = tempItems[i];
+				items[index] = item;
+				items[index+1] = tempItems[i];
+				index++;
+				i++;
+				
+				
+//				index++;
+//				i++;
+			}
+			
+//			
+//		} else if (index > sizeOfList) {
+//			System.err.println("Index Out of Bounds Exception");
+//		}
+		
+		return false;
+	}
+	
+	
+	@Override
+	public int getSize() {
+		
+		if (sizeOfList == 0) {
+			System.out.println("The list is currently empty");
+		}
+		return sizeOfList;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public T get(int index) {
+		
+		if (index >= getSize() || index < 0)  {
+			System.out.println("Index is out of bounds");
+		} else {
+			return (T) items[index];
+		}
+		return null;
+	}
+
+
+	@Override
+	public T remove(int index) throws IndexOutOfBoundsException {
+		
+//		if (items[index] != null) {
+//			items[index] = 
+//		}
+		return null;
+	}
+	
+}
---
+++ src/com/fcynnek/assignment7/CustomListApplication.java
@@ -0,0 +1,45 @@
+package com.fcynnek.assignment7;
+
+import com.coderscampus.arraylist.CustomArrayList;
+import com.coderscampus.arraylist.CustomList;
+
+public class CustomListApplication {
+
+	public static void main(String[] args) {
+
+		// TESTING THE CODE:
+		CustomList<String> myCustomList = new CustomArrayList<>();
+
+		myCustomList.add("element 1"); // then add 10, 20, or 40 elements
+		myCustomList.add("element 2");
+		myCustomList.add("element 3");
+		myCustomList.add("element 4");
+		myCustomList.add("element 5");
+		myCustomList.add("element 6");
+		myCustomList.add("element 7");
+		myCustomList.add("element 8");
+		myCustomList.add("element 9");
+		myCustomList.add("element 10");
+		myCustomList.add("element 11"); // index = 10
+		myCustomList.add("element 12");
+		myCustomList.add("element 13");
+		myCustomList.add("element 14");
+		myCustomList.add("element 15");
+		myCustomList.add("element 16");
+		myCustomList.add("element 17");
+		myCustomList.add("element 18");
+		myCustomList.add("element 19");
+		myCustomList.add("element 20");
+		myCustomList.add("element 21");
+		myCustomList.add(11, "new element 11");
+
+		System.out.println(myCustomList.getSize());
+
+		// then validate that all the elements exist in the data structure
+		for (int i = 0; i < myCustomList.getSize(); i++) {
+			System.out.println(myCustomList.get(i));
+		}
+	}
+
+
+}
