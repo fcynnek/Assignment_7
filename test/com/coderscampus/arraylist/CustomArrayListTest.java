3697bd7a81dcf914991c4a3969abf47394255c9d
fcynnek <kchengfung@gmail.com>
2023-02-09 19:04:25 -0500

    Test

--- test/com/coderscampus/arraylist/CustomArrayListTest.java
+++ test/com/coderscampus/arraylist/CustomArrayListTest.java
@@ -7,8 +7,50 @@ import org.junit.jupiter.api.Test;
 class CustomArrayListTest {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void testAddMethod() {
+		CustomArrayList<Integer> list = new CustomArrayList<>();
+		assertEquals(false, list.add(1));
+		assertEquals(1, list.getSize());
+	}
+	
+	@Test
+	public void testAddAtIndexMethod() {
+		CustomArrayList<String> list = new CustomArrayList<>();
+		list.add("Hello");
+		assertEquals(false, list.add(0, "World"));
+		assertEquals("World", list.get(0));
+		assertEquals("Hello", list.get(1));
+		assertEquals(2, list.getSize());
+	}
+	
+	@Test
+	public void testGetMethod() {
+		CustomArrayList<Double> list = new CustomArrayList<>();
+		list.add(3.14);
+		assertEquals(3.14, list.get(0), 0);
+		assertEquals(1, list.getSize());
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testGetMethodWithInvalidIndex() {
+		CustomArrayList<Boolean> list = new CustomArrayList<>();
+		list.get(0);
+	}
+	
+	@Test
+	public void testRemoveMethod() {
+		CustomArrayList<Character> list = new CustomArrayList<>();
+		list.add('A');
+		list.add('B');
+		list.remove(0);
+		assertEquals('B', list.get(0));
+		assertEquals(1, list.getSize());
+	}
+	
+	@Test(expected = IndexOutOfBoundsException.class)
+	public void testRemoveMethodWithInvalidIndex() {
+		CustomArrayList<Integer> list = new CustomArrayList<>();
+		list.remove(0);
 	}
 
 }
